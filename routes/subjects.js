e6f625b758c531d79ac9d9af084eef66ecffefd4
svenbrodny <sven.brodny@piobyte.de>
2020-05-24 11:46:36 +0200

    fix: database connection name - 'posts' instead of 'test'

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/subjects', (req, res) => {
+router.get('/', (req, res) => {
     res.send('We are on subjects');
 });
 
