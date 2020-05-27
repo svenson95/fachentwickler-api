c36f5ce20cf7e58a35516152d06b8d6ba565585a
svenbrodny <sven.brodny@piobyte.de>
2020-05-27 21:06:34 +0200

    fix: wrong postsTitles class declaration (2)

--- models/PostTitles.js
+++ models/PostTitles.js
@@ -8,14 +8,16 @@ const connection = mongoose.createConnection(process.env.DB_CONNECTION_POSTS, {
 const PostTitles = connection.model('postsTitles', new mongoose.Schema({
     subject: { type: String, required: true },
     topics: { type: Array, default: [
-            {
-                title: { type: String, required: true },
-                links: Array, default: [
-                    { title: String, description: String, url: String },
-                ]
-            }
-        ]},
-    tests: { type: Array, required: false }
+        {
+            title: String,
+            links: { type: Array, default: [
+                { title: String, description: String, url: String }
+            ]}
+        }
+    ]},
+    tests: { type: Array, default: [
+        { title: String, description: String, url: String }
+    ]}
 }));
 
 module.exports = PostTitles;
