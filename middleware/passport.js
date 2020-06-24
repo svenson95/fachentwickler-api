a71d791e9a5acc25027259c577312afde102e04c
svenbrodny <sven.brodny@piobyte.de>
2020-06-24 17:52:42 +0200

    fix: wrong reference for login system

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
-const User = require('./models/User');
+const User = require('../models/user/User');
 
 const cookieExtractor = req => {
     let token = null;
