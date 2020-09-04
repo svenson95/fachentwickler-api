c1cbde4081290101de8b7ea29871476bc324c6cb
Sven Brodny <sven.brodny@piobyte.de>
2020-09-05 01:36:50 +0200

    feat: api for get subjectPost by post object _id

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Subjects = require('../models/subject/Subject');
+const Posts = require('../models/posts/Posts');
+const Quizzes = require('../models/quiz/Quiz');
 
 router.get('/', async (req, res) => {
     try {
@@ -42,6 +44,18 @@ router.get('/:subject', async (req, res) => {
     } catch (error) {
         res.json({ message: error });
     }
+});
+
+// Get specific subjectPost by post _id
+router.get('/post/:postId', async (req, res) => {
+    let post = await Posts.findOne({ "_id": req.params.postId });
+    if (post === null) post = await Quizzes.findOne({ "_id": req.params.postId });
+    const subject = await Subjects.findOne({ subject: post.subject });
+    const topic = subject.topics.find(topic => topic.links.find(link => link.postId === req.params.postId));
+    let subPost = topic?.links.find(el => el.postId === req.params.postId);
+    if (!subPost) subPost = subject.tests.find(test => test.postId === req.params.postId);
+    subPost.subject = subject.subject;
+    res.json(subPost);
 });
 
 // Delete specific subject
