88b97d86293b9640207d9a278a7e06ee1a6a05bc
Sven Brodny <sven.brodny@piobyte.de>
2020-10-22 00:06:44 +0200

    updated quiz model - missing properties 'schoolWeek', ... 'subject' and 'topic'

--- models/quiz/Quiz.js
+++ models/quiz/Quiz.js
@@ -6,18 +6,20 @@ const connection = mongoose.createConnection(process.env.DB_CONNECTION_SCHOOLBAS
 });
 
 const Quiz = connection.model('quizzes', new mongoose.Schema({
-    url:        { type: String, required: true },
+    url: { type: String, required: true },
+    subject: { type: String, required: true },
+    topic: { type: String, required: true },
+    schoolWeek: { type: String, required: true },
     lessonDate: { type: String, required: true },
     lastUpdate: { type: String, required: true },
-    questions:  { type: Array, default: [
+    questions: { type: Array, default: [
         {
             question: { type: String, required: true },
             choice1: { type: String, required: true },
             choice2: { type: String, required: true },
             answer: { type: Number, required: true }
         }
-    ]},
-    subject: { type: String, required: true }
+    ]}
 }));
 
 module.exports = Quiz;
